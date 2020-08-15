53cda2244da2e5d04a290f96fcafe3d41a2ab08b
michael00987 <hyeongkwangseok@gmail.com>
2020-08-16 01:40:25 +0900

    별찍기 객체 형태로 수정

--- 03.drawStars/index.js
+++ 03.drawStars/index.js
@@ -1,48 +1,65 @@
-case0;
-
-for (let star = 1; star <= 16; star += 1) {
-  console.log('*'.repeat(star));
-}
-case1;
-
-for (let star = 1; star <= 16; star += 2) {
-  console.log('*'.repeat(star));
-}
-
-case2;
-
-for (let star = 1; star <= 16; star *= 2) {
-  console.log('*'.repeat(star));
-}
-
-case3;
-
-for (let i = 0; i < 10; i++) {
-  console.log(' '.repeat(10 - i), '*'.repeat(i));
-}
-
-case4;
-
-for (let i = 10; i > 0; i--) {
-  console.log(' '.repeat(10 - i), '*'.repeat(i));
-}
-
-case5;
-
-for (let i = 10; i > 0; i -= 2) {
-  console.log(' '.repeat(10 - i / 2), '*'.repeat(i), ' '.repeat(10 - i / 2));
-}
-case6;
-
-for (let i = 9; i > 0; i -= 2) {
-  console.log(' '.repeat(10 - i / 2), '*'.repeat(i), ' '.repeat(10 - i / 2));
-}
-
-case6;
-
-for (let i = 1; i < 5; i += 2) {
-  console.log(' '.repeat(10 - i / 2), '*'.repeat(i), ' '.repeat(10 - i / 2));
-}
-for (let i = 5; i > 0; i -= 2) {
-  console.log(' '.repeat(10 - i / 2), '*'.repeat(i), ' '.repeat(10 - i / 2));
+const star = {
+  case0: function () {
+    for (let star = 1; star <= 16; star += 1) {
+      console.log('*'.repeat(star));
+    }
+  },
+  case1: function () {
+    for (let star = 1; star <= 16; star += 2) {
+      console.log('*'.repeat(star));
+    }
+  },
+  case2: function () {
+    for (let star = 1; star <= 16; star *= 2) {
+      console.log('*'.repeat(star));
+    }
+  },
+  case3: function () {
+    for (let i = 0; i < 10; i++) {
+      console.log(' '.repeat(10 - i), '*'.repeat(i));
+    }
+  },
+  case4: function () {
+    for (let i = 10; i > 0; i--) {
+      console.log(' '.repeat(10 - i), '*'.repeat(i));
+    }
+  },
+  case5: function () {
+    for (let i = 10; i > 0; i -= 2) {
+      console.log(
+        ' '.repeat(10 - i / 2),
+        '*'.repeat(i),
+        ' '.repeat(10 - i / 2)
+      );
+    }
+  },
+  case6: function () {
+    for (let i = 9; i > 0; i -= 2) {
+      console.log(
+        ' '.repeat(10 - i / 2),
+        '*'.repeat(i),
+        ' '.repeat(10 - i / 2)
+      );
+    }
+  },
+  case6: function () {
+    for (let i = 1; i < 5; i += 2) {
+      console.log(
+        ' '.repeat(10 - i / 2),
+        '*'.repeat(i),
+        ' '.repeat(10 - i / 2)
+      );
+      for (let i = 5; i > 0; i -= 2) {
+        console.log(
+          ' '.repeat(10 - i / 2),
+          '*'.repeat(i),
+          ' '.repeat(10 - i / 2)
+        );
+      }
+    }
+  },
+};
+for (let i = 0; i < Object.keys(star).length; i++) {
+  console.log(`case${i}\n`);
+  console.log(star[`case${i}`]());
 }
