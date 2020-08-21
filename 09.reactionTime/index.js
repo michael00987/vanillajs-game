f012c22752dd5392202a2573c7fda27049cdb236
michael00987 <hyeongkwangseok@gmail.com>
2020-08-21 23:54:40 +0900

    예외 상황 처리

--- 09.reactionTime/index.js
+++ 09.reactionTime/index.js
@@ -1,28 +1,42 @@
 const screen = document.querySelector('#screen');
 
-const status = {};
-let startTime = 0;
-let endTime = 0;
+const avg = [];
+let startTime = null;
+let endTime = null;
+let timeOut;
 screen.addEventListener('click', () => {
   if (screen.classList.contains('waiting')) {
     screen.classList.remove('waiting');
     screen.classList.add('ready');
     screen.textContent = '초록색이 되면 클릭하세요';
-    setTimeout(() => {
-      screen.click();
+    timeOut = setTimeout(() => {
       startTime = new Date();
-      console.log('지금이야!!');
+      screen.click();
     }, Math.floor(Math.random() * 1000) + 2000);
-    console.log('준비');
   } else if (screen.classList.contains('ready')) {
-    screen.classList.remove('ready');
-    screen.classList.add('now');
-    screen.textContent = '클릭하세요';
+    if (!startTime) {
+      clearTimeout(timeOut);
+      screen.classList.remove('ready');
+      screen.classList.add('waiting');
+      screen.textContent = '너무 성급하시군요';
+    } else {
+      screen.classList.remove('ready');
+      screen.classList.add('now');
+      screen.textContent = '클릭하세요';
+    }
   } else if (screen.classList.contains('now')) {
     endTime = new Date();
     console.log(endTime - startTime + 'ms');
+    avg.push(endTime - startTime);
+    const avgNum = avg.reduce((acc, curr) => {
+      return (acc += curr);
+    });
+    console.log(avgNum, 'avgNum');
+    document.querySelector('#result').textContent = avgNum / avg.length;
     screen.classList.remove('now');
     screen.classList.add('waiting');
     screen.textContent = '클릭해서 시작하세요';
+    startTime = null;
+    endTime = null;
   }
 });
