5ffa279003b9e1e02f05d1c9d302cac2697532e2
michael00987 <hyeongkwangseok@gmail.com>
2020-08-21 23:24:51 +0900

    속도 체크

--- 09.reactionTime/index.js
+++ 09.reactionTime/index.js
@@ -1,18 +1,26 @@
 const screen = document.querySelector('#screen');
 
 const status = {};
-
+let startTime = 0;
+let endTime = 0;
 screen.addEventListener('click', () => {
   if (screen.classList.contains('waiting')) {
     screen.classList.remove('waiting');
     screen.classList.add('ready');
     screen.textContent = '초록색이 되면 클릭하세요';
+    setTimeout(() => {
+      screen.click();
+      startTime = new Date();
+      console.log('지금이야!!');
+    }, Math.floor(Math.random() * 1000) + 2000);
     console.log('준비');
   } else if (screen.classList.contains('ready')) {
     screen.classList.remove('ready');
     screen.classList.add('now');
     screen.textContent = '클릭하세요';
   } else if (screen.classList.contains('now')) {
+    endTime = new Date();
+    console.log(endTime - startTime + 'ms');
     screen.classList.remove('now');
     screen.classList.add('waiting');
     screen.textContent = '클릭해서 시작하세요';
