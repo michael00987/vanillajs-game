060235dc426e86518e98b955933effc4f2286c6b
michael <hyeongkwangseok@gmail.com>
2020-08-23 02:38:59 +0900

    카드 셋팅

--- 10.toggleCard/index.js
+++ 10.toggleCard/index.js
@@ -1,5 +1,11 @@
 const ver = 4;
 const hor = 3;
+let clickFlag = true
+const candidateColor=['red','red','yellow','yellow','green','green','orange','orange','white','white','pink','pink']
+let color=[]
+for(let i = 0; candidateColor.length > 0; i++ ){
+  color=color.concat(candidateColor.splice(Math.floor(Math.random()*candidateColor.length),1));
+}
 let settingCard = (ver, hor) => {
   for (let i = 0; i < hor * ver; i++) {
     let card = document.createElement('div');
@@ -10,14 +16,30 @@ let settingCard = (ver, hor) => {
     cardFront.className = 'card-front';
     let cardBack = document.createElement('div');
     cardBack.className = 'card-back';
+    cardBack.style.backgroundColor=color[i]
     cardInner.appendChild(cardFront);
     cardInner.appendChild(cardBack);
     card.appendChild(cardInner);
     card.addEventListener('click', () => {
-      card.classList.toggle('flipped');
+      if(clickFlag){
+        card.classList.toggle('flipped');
+      }
     });
     document.body.appendChild(card);
   }
+  document.querySelectorAll('.card').forEach((card,index)=>{
+    setTimeout(()=>{
+      clickFlag=false
+      card.classList.toggle('flipped')
+    },1000+100*index)
+  })
+  document.querySelectorAll('.card').forEach((card,index)=>{
+    setTimeout(()=>{
+      card.classList.toggle('flipped')
+      clickFlag=true
+    },5000)
+  })
+
 };
 
 settingCard(ver, hor);
